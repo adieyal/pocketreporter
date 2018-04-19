08bda0dbb73c3642e81fc6a61e309002ecef40e0
Adi Eyal <adi@openup.org.za>
2018-04-19 13:45:11 +0200

    Removed old crud

--- js/main.js
+++ js/main.js
@@ -255,8 +255,6 @@ var app = {
     PocketReporter = new PocketReporter();
     router = new Router();
     Backbone.history.start();
-    if ('ga' in window) window.ga.startTrackerWithId('UA-48399585-49');
-    console.log('Event received: ',id);
   }
 };
 
